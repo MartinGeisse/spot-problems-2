5c65400bdc651a9ce2bde42df5142b79eaa16406
Martin Geisse <martin.geisse@janitza.de>
2025-11-09 11:29:36 +0100

    ...

--- src/content/math/induction/createInductionExercise.tsx
+++ src/content/math/induction/createInductionExercise.tsx
@@ -77,31 +77,136 @@ function natInductionExercise(
 
 // nothing is randomized in the instances, only the choice of instance is
 const exerciseInstances: ExerciseInstance[] = [
+  // natInductionExercise(
+  //     <>{mathSpan("n^2+n")} is even</>,
+  //     mathDiv("n^2+n = 1^2+1 = 1+1 = 2"),
+  //     <>{mathSpan("(n+1)^2+(n+1)")} is even</>,
+  //     _detailLevel => <>
+  //       {mathDiv("(n+1)^2 + (n+1)")}
+  //       {mathDiv("= n^2 + 2n + 1 + n + 1")}
+  //       {mathDiv("= n^2 + 3n + 2")}
+  //       {mathDiv("= (n^2 + n) + (2n + 2)")}
+  //       {mathDiv("= (n^2 + n) + 2#cdot (n + 1)")}
+  //       <div>The first part is even by the induction hypothesis, so the sum is even too.</div>
+  //     </>,
+  // ),
+
+  // natInductionExercise(
+  //     <>{mathSpan("n^3+2n")} is divisible by {mathSpan("3")}</>,
+  //     mathDiv("n^3+2n = 1^3+2#cdot 1 = 1 + 2 = 3"),
+  //     <>{mathSpan("(n+1)^3+2#cdot (n+1)")} is divisible by {mathSpan("3")}</>,
+  //     _detailLevel => <>
+  //       {mathDiv("(n+1)^3 + 2#cdot (n+1)")}
+  //       {mathDiv("= n^3 + 3n^2 + 3n + 1 + 2n + 2")}
+  //       {mathDiv("= n^3 + 3n^2 + 5n + 3")}
+  //       {mathDiv("= (n^3 + 2n) + (3n^2 + 3n + 3)")}
+  //       <div>The first part is divisible by 3 by the induction hypothesis, so the sum is divisible by 3 too.</div>
+  //     </>,
+  // ),
+
+  // natInductionExercise(
+  //     <>{mathSpan("4n^3-n")} is divisible by {mathSpan("3")}</>,
+  //     mathDiv("4n^3-n = 4#cdot 1^3 - 1 = 4 - 1 = 3"),
+  //     <>{mathSpan("4(n+1)^3-(n+1)")} is divisible by {mathSpan("3")}</>,
+  //     _detailLevel => <>
+  //       {mathDiv("4(n+1)^3-(n+1)")}
+  //       {mathDiv("= 4#cdot (n^3 + 3n^2 + 3n + 1) - n - 1")}
+  //       {mathDiv("= 4n^3 + 12n^2 + 12n + 4 - n - 1")}
+  //       {mathDiv("= 4n^3 - n + 12n^2 + 12n + 3")}
+  //       {mathDiv("= (4n^3 - n) + 3#cdot (4n^2 + 4n + 1)")}
+  //       <div>The first part is divisible by 3 by the induction hypothesis, so the sum is divisible by 3 too.</div>
+  //     </>,
+  // ),
+
+  // natInductionExercise(
+  //     <>{mathSpan("n^3-n")} is divisible by {mathSpan("6")}</>,
+  //     mathDiv("n^3-n = 1^3-1 = 0"),
+  //     <>{mathSpan("(n+1)^3 - (n+1)")} is divisible by {mathSpan("6")}</>,
+  //     _detailLevel => <>
+  //       {mathDiv("(n+1)^3 - (n+1)")}
+  //       {mathDiv("= n^3 + 3n^2 + 3n + 1 - n - 1")}
+  //       {mathDiv("= n^3 - n + 3n^2 + 3n")}
+  //       {mathDiv("= (n^3 - n) + 3n(n + 1)")}
+  //       <div>The first part is divisible by 6 by the induction hypothesis. The second part is divisible by 6 because
+  //         of the obvious factor of {mathSpan("3")}, as well as the fact that either {mathSpan("n")} or
+  //         {mathSpan("(n+1)")} is even.</div>
+  //     </>,
+  // ),
+
+  // natInductionExercise(
+  //     <>{mathSpan("2n^3+3n^2+n")} is divisible by {mathSpan("6")}</>,
+  //     mathDiv("2n^3+3n^2+n = 2#cdot 1^3 + 3#cdot 1^2 + 1 = 2 + 3 + 1 = 6"),
+  //     <>{mathSpan("2(n+1)^3+3(n+1)^2+(n+1)")} is divisible by {mathSpan("6")}</>,
+  //     _detailLevel => <>
+  //       {mathDiv("2(n+1)^3+3(n+1)^2+(n+1)")}
+  //       {mathDiv("= 2(n^3 + 3n^2 + 3n + 1) + 3(n^2 + 2n + 1) + n + 1")}
+  //       {mathDiv("= 2n^3 + 6n^2 + 6n + 2 + 3n^2 + 6n + 3 + n + 1")}
+  //       {mathDiv("= 2n^3 + 3n^2 + n + 6n^2 + 6n + 2 + 6n + 3 + 1")}
+  //       {mathDiv("= (2n^3 + 3n^2 + n) + (6n^2 + 12n + 6)")}
+  //       {mathDiv("= (2n^3 + 3n^2 + n) + 6(n^2 + 2n + 1)")}
+  //       <div>The first part is divisible by 6 by the induction hypothesis, so the sum is divisible by 6 too.</div>
+  //     </>,
+  // ),
+
+  // natInductionExercise(
+  //     <>{mathSpan("n^3-6n^2+14n")} is divisible by {mathSpan("3")}</>,
+  //     mathDiv("n^3-6n^2+14n = 1^3-6#cdot 1^2+14#cdot 1 = 1 - 6 + 14 = 9"),
+  //     <>{mathSpan("(n+1)^3-6(n+1)^2+14(n+1)")} is divisible by {mathSpan("3")}</>,
+  //     _detailLevel => <>
+  //       {mathDiv("(n+1)^3-6(n+1)^2+14(n+1)")}
+  //       {mathDiv("= (n^3 + 3n^2 + 3n + 1) - 6(n^2 + 2n + 1) + (14n + 14)")}
+  //       {mathDiv("= n^3 + 3n^2 + 3n + 1 - 6n^2 - 12n - 6 + 14n + 14")}
+  //       {mathDiv("= n^3 - 6n^2 + 14n + 3n^2 + 3n + 1 - 12n - 6 + 14")}
+  //       {mathDiv("= (n^3 - 6n^2 + 14n) + (3n^2 + - 9n + 9)")}
+  //       {mathDiv("= (n^3 - 6n^2 + 14n) + 3(n^2 + - 3n + 3)")}
+  //       <div>The first part is divisible by 3 by the induction hypothesis, so the sum is divisible by 3 too.</div>
+  //     </>,
+  // ),
+
+  // natInductionExercise(
+  //     <>{mathSpan("3^n - 3")} is divisible by {mathSpan("6")}</>,
+  //     mathDiv("3^n - 3 = 3^1 - 3 = 3 - 3 = 0"),
+  //     <>{mathSpan("3^{n+1} - 3")} is divisible by {mathSpan("6")}</>,
+  //     _detailLevel => <>
+  //       {mathDiv("3^{n+1} - 3")}
+  //       {mathDiv("= (3#cdot 3^n) + (6 - 9)")}
+  //       {mathDiv("= (3#cdot 3^n - 9) + 6")}
+  //       {mathDiv("= 3#cdot (3^n - 3) + 6")}
+  //       <div>The first part is divisible by 6 by the induction hypothesis, so the sum is divisible by 6 too.</div>
+  //     </>,
+  // ),
+
   natInductionExercise(
-      <>{mathSpan("n^2+n")} is even</>,
-      mathDiv("n^2+n = 1^2+1 = 1+1 = 2"),
-      <>{mathSpan("(n+1)^2+(n+1)")} is even</>,
+      <>{mathSpan("n^3 + (n+1)^3 + (n+2)^3")} is divisible by {mathSpan("9")}</>,
+      mathDiv("n^3 + (n+1)^3 + (n+2)^3 = 1^3 + (1+1)^3 + (1+2)^3 = 1 + 2^3 + 3^3 = 1 + 8 + 27 = 36"),
+      <>{mathSpan("(n+1)^3 + ((n+1)+1)^3 + ((n+1)+2)^3")} is divisible by {mathSpan("9")}</>,
       _detailLevel => <>
-        {mathDiv("(n+1)^2 + (n+1)")}
-        {mathDiv("= n^2 + 2n + 1 + n + 1")}
-        {mathDiv("= n^2 + 3n + 2")}
-        {mathDiv("= (n^2 + n) + (2n + 2)")}
-        {mathDiv("= (n^2 + n) + 2#cdot (n + 1)")}
-        <div>The first part is even by the induction hypothesis, so the sum is even too.</div>
+        {mathDiv("(n+1)^3 + ((n+1)+1)^3 + ((n+1)+2)^3")}
+        {mathDiv("= (n+1)^3 + (n+2)^3 + (n+3)^3")}
+        {mathDiv("= (n^3 + 3n^2 + 3n + 1) + (n^3 + 6n^2 + 12n + 8) + (n^3 + 9n^2 + 27n + 27)")}
+        {mathDiv("= n^3 + (n^3 + 3n^2 + 3n + 1) + (n^3 + 6n^2 + 12n + 8) + (9n^2 + 27n + 27)")}
+        {mathDiv("= (n^3 + (n+1)^3 (n+2)^3) + 9(n^2 + 3n + 3)")}
+        <div>The first part is divisible by 9 by the induction hypothesis, so the sum is divisible by 9 too.</div>
       </>,
   ),
-  natInductionExercise(
-      <>{mathSpan("n^3+2n")} is divisible by {mathSpan("3")}</>,
-      mathDiv("n^3+2n = 1^3+2#cdot 1 = 1 + 2 = 3"),
-      <>{mathSpan("(n+1)^3+2#cdot (n+1)")} is divisible by {mathSpan("3")}</>,
-      _detailLevel => <>
-        {mathDiv("(n+1)^3 + 2#cdot (n+1)")}
-        {mathDiv("= n^3 + 3n^2 + 3n + 1 + 2n + 2")}
-        {mathDiv("= n^3 + 3n^2 + 5n + 3")}
-        {mathDiv("= (n^3 + 2n) + (3n^2 + 3n + 3)")}
-        <div>The first part is divisible by 3 by the induction hypothesis, so the sum is divisible by 3 too.</div>
-      </>,
-  ),
+    
+    
+    
+    
+
+
+  // natInductionExercise(
+  //     <>{mathSpan("n^3+2n")} is divisible by {mathSpan("3")}</>,
+  //     mathDiv("n^3+2n = 1^3+2#cdot 1 = 1 + 2 = 3"),
+  //     <>{mathSpan("(n+1)^3+2#cdot (n+1)")} is divisible by {mathSpan("3")}</>,
+  //     _detailLevel => <>
+  //       {mathDiv("(n+1)^3 + 2#cdot (n+1)")}
+  //       {mathDiv("= n^3 + 3n^2 + 3n + 1 + 2n + 2")}
+  //       {mathDiv("= n^3 + 3n^2 + 5n + 3")}
+  //       {mathDiv("= (n^3 + 2n) + (3n^2 + 3n + 3)")}
+  //       <div>The first part is divisible by 3 by the induction hypothesis, so the sum is divisible by 3 too.</div>
+  //     </>,
+  // ),
 ];
 
 export function createInductionExercise(): ExerciseInstance {
