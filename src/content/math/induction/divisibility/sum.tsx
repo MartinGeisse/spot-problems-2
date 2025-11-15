e29f3b4663a5b821a7cba03a3a7b29c9222d54ef
Martin Geisse <martin.geisse@janitza.de>
2025-11-15 11:55:06 +0100

    ...

--- src/content/math/induction/divisibility/sum.tsx
+++ src/content/math/induction/divisibility/sum.tsx
@@ -56,28 +56,42 @@ export const sumExerciseInstances: ExerciseInstance[] = [
   //     </>
   // ),
 
+  // natInductionExercise(
+  //     <>{mathSpan("1^4+2^4+3^4+...+n^4 = #sum_{i=1}^ni^4 = #frac{n(n+1)(2n+1)(3n^2+3n-1)}{30}")}</>,
+  //     <>
+  //       {mathDiv("#sum_{i=1}^ni^4 = #sum_{i=1}^1i^4 = 1^4 = 1 = #frac{30}{30}")}
+  //       {mathDiv("= #frac{1#cdot 2#cdot 3#cdot 5}{30}")}
+  //       {mathDiv("= #frac{1(1+1)(2#cdot 1+1)(3#cdot 1^2+3#cdot 1-1)}{30}")}
+  //       {mathDiv("= #frac{n(n+1)(2n+1)(3n^2+3n-1)}{30}")}
+  //     </>,
+  //     mathSpan("#sum_{i=1}^{n+1}i^4 = #frac{(n+1)((n+1)+1)(2(n+1)+1)(3(n+1)^2+3(n+1)-1)}{30}"),
+  //     _detailLevel => <>
+  //       {mathDiv("#sum_{i=1}^{n+1}i^4")}
+  //       {mathDiv("= (n+1)^4 + #sum_{i=1}^ni^4")}
+  //       <div>using the induction hypothesis:</div>
+  //       {mathDiv("= (n+1)^4 + #frac{n(n+1)(2n+1)(3n^2+3n-1)}{30}")}
+  //       {mathDiv("= #frac{30(n+1)^4}{30} + #frac{n(n+1)(2n+1)(3n^2+3n-1)}{30}")}
+  //       {mathDiv("= #frac{30(n+1)^4 + n(n+1)(2n+1)(3n^2+3n-1)}{30}")}
+  //       {mathDiv("= #frac{n+1}{30}(30(n+1)^3 + n(2n+1)(3n^2+3n-1))")}
+  //       <div style={{fontSize: "0.87em"}}>{mathDiv("= #frac{n+1}{30}(30n^3 + 90n^2 + 90n + 30 + 6n^4 + 6n^3 - 2n^2 + 3n^3 + 3n^2 - n)")}</div>
+  //       {mathDiv("= #frac{n+1}{30}(6n^4 + 39n^3 + 91n^2 + 89n + 30)")}
+  //       {mathDiv("= #frac{(n+1)(n+2)}{30}(6n^3 + 27n^2 + 37n + 15)")}
+  //       {mathDiv("= #frac{(n+1)(n+2)}{30}(2n + 3)(3n^2 + 9n + 5)")}
+  //       <div style={{fontSize: "0.9em"}}>{mathDiv("= #frac{(n+1)((n+1)+1)(2(n+1)+1)(3(n+1)^2+3(n+1)-1)}{30}")}</div>
+  //     </>
+  // ),
+
   natInductionExercise(
-      <>{mathSpan("1^4+2^4+3^4+...+n^4 = #sum_{i=1}^ni^4 = #frac{n(n+1)(2n+1)(3n^2+3n-1)}{30}")}</>,
-      <>
-        {mathDiv("#sum_{i=1}^ni^4 = #sum_{i=1}^1i^4 = 1^4 = 1 = #frac{30}{30}")}
-        {mathDiv("= #frac{1#cdot 2#cdot 3#cdot 5}{30}")}
-        {mathDiv("= #frac{1(1+1)(2#cdot 1+1)(3#cdot 1^2+3#cdot 1-1)}{30}")}
-        {mathDiv("= #frac{n(n+1)(2n+1)(3n^2+3n-1)}{30}")}
-      </>,
-      mathSpan("#sum_{i=1}^{n+1}i^4 = #frac{(n+1)((n+1)+1)(2(n+1)+1)(3(n+1)^2+3(n+1)-1)}{30}"),
+      <>{mathSpan("1+3+5+...+(2n-1) = #sum_{i=1}^n(2i-1) = n^2")}</>,
+      <>{mathDiv("#sum_{i=1}^n(2i-1) = #sum_{i=1}^1(2i-1) = 2#cdot 1 - 1 = 1 = 1^2")}</>,
+      mathSpan("#sum_{i=1}^{n+1}(2i-1) = (n+1)^2"),
       _detailLevel => <>
-        {mathDiv("#sum_{i=1}^{n+1}i^4")}
-        {mathDiv("= (n+1)^4 + #sum_{i=1}^ni^4")}
+        {mathDiv("#sum_{i=1}^{n+1}(2i-1)")}
+        {mathDiv("= (2(n+1)-1) + #sum_{i=1}^n(2i-1)")}
+        {mathDiv("= (2n+1) + #sum_{i=1}^n(2i-1)")}
         <div>using the induction hypothesis:</div>
-        {mathDiv("= (n+1)^4 + #frac{n(n+1)(2n+1)(3n^2+3n-1)}{30}")}
-        {mathDiv("= #frac{30(n+1)^4}{30} + #frac{n(n+1)(2n+1)(3n^2+3n-1)}{30}")}
-        {mathDiv("= #frac{30(n+1)^4 + n(n+1)(2n+1)(3n^2+3n-1)}{30}")}
-        {mathDiv("= #frac{n+1}{30}(30(n+1)^3 + n(2n+1)(3n^2+3n-1))")}
-        <div style={{fontSize: "0.87em"}}>{mathDiv("= #frac{n+1}{30}(30n^3 + 90n^2 + 90n + 30 + 6n^4 + 6n^3 - 2n^2 + 3n^3 + 3n^2 - n)")}</div>
-        {mathDiv("= #frac{n+1}{30}(6n^4 + 39n^3 + 91n^2 + 89n + 30)")}
-        {mathDiv("= #frac{(n+1)(n+2)}{30}(6n^3 + 27n^2 + 37n + 15)")}
-        {mathDiv("= #frac{(n+1)(n+2)}{30}(2n + 3)(3n^2 + 9n + 5)")}
-        <div style={{fontSize: "0.9em"}}>{mathDiv("= #frac{(n+1)((n+1)+1)(2(n+1)+1)(3(n+1)^2+3(n+1)-1)}{30}")}</div>
+        {mathDiv("= 2n + 1 + n^2")}
+        {mathDiv("= (n+1)^2")}
       </>
   ),
 
