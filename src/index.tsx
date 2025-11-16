1b253284454428cc1d491ee289f9521b1febb67b
Martin Geisse <martin.geisse@janitza.de>
2025-11-16 14:55:15 +0100

    ...

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client"
 import {App} from "./framework/technical-components/App/App";
 import "katex/dist/katex.min.css";
 import {initializeDeveloperControls} from "./framework/technical-components/App/developer";
@@ -7,7 +6,4 @@ import "./framework/technical-components/App/global.css"
 
 initializeDeveloperControls();
 
-const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
-);
-root.render(<App/>);
+createRoot(document.getElementById("root") as HTMLElement).render(<App/>);
